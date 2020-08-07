196e1dc62361d110787ab6304ff63bcf4b9fe30a
Hanke <hannes.knutsson96@gmail.com>
2020-08-07 23:11:17 +0200

    Check user before attempting install of packages

--- install_packages.bash
+++ install_packages.bash
@@ -3,18 +3,29 @@
 #	Installs a set of packages defined in the file "packages" using whatever package manager is available.
 #
 
+if [ "$(whoami)" != "root" ]
+then
+	echo -e "\t\nNot running as root. Canceling...\n"
+	exit 1
+fi
+
+PACKAGES=`cat packages`
+PACKAGES_NICE="${PACKAGES//$'\n'/, }"
+
+echo -e "\n\t* Packages to install: $PACKAGES_NICE"
+
 if [[ `command -v apt` ]]
 then
 	echo -e "\n\t* apt detected. Installing packages using apt...\n"
-	apt install -y `cat packages`
+	apt install -y $PACKAGES
 elif [[ `command -v yum` ]]
 then
 	echo -e "\n\t* yum detected. Installing packages using yum...\n"
-	yum install -y --skip-broken `cat packages`
+	yum install -y --skip-broken $PACKAGES
 elif [[ `command -v pacman` ]]
 then
 	echo -e "\n\t* pacman detected. Installing packages using pacman...\n"
-	pacman -S `cat packages` --noconfirm
+	pacman -S --noconfirm $PACKAGES
 else
 	echo -e "\n\t* Could not find any package manager. Canceling...\n"
 fi
