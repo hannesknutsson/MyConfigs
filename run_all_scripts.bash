2e0085b2e96068a0505b9e6ce99982a875dcbed4
Hanke <hannes.knutsson@protonmail.com>
2020-12-25 21:19:55 +0000

    Fix bug in run all script

--- run_all_scripts.bash
+++ run_all_scripts.bash
@@ -2,6 +2,6 @@
 
 echo "Script may prompt for your password to be able to install packages as super user..."\
 && sudo ./install_packages.bash \
-&& ./install_oh-my-zsh.bash
-&& ./setup.bash
-
+&& ./install_oh-my-zsh.bash \
+&& ./setup.bash \
+zsh
