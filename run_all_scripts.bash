fe343c55e7b5a5d5aa43a1b8d256b361cd01185c
Hanke <hannes.knutsson@protonmail.com>
2020-12-26 10:12:09 +0000

    Make major structure improvements

--- run_all_scripts.bash
+++
@@ -1,7 +0,0 @@
-#/bin/bash
-
-echo "Script may prompt for your password to be able to install packages as super user..."\
-&& sudo ./install_packages.bash \
-&& ./install_oh-my-zsh.bash \
-&& ./setup.bash \
-zsh
