924101b8fb5b01368908263ac1831cb7870b83f0
Hannes Knutsson <hannes.knutsson@protonmail.com>
2021-11-18 19:20:39 +0100

    Add desktop entry for YubiOAUTH

--- scripts/install_appimages.bash
+++ scripts/install_appimages.bash
@@ -8,6 +8,8 @@ echo -e '\t* AppImages to install: YubiOAUTH'
 
 #Yubikey OAUTH client
 wget -4 -q -O $APPIMG_DIR/yubioauth https://developers.yubico.com/yubioath-desktop/Releases/yubioath-desktop-latest-linux.AppImage
+mkdir -p $HOME/.local/share/applications
+cp desktop-entries/* $HOME/.local/share/applications/
 
 chmod u+x $APPIMG_DIR/*
 
