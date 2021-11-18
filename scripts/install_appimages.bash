f5b80105d2a781efcfd65f6a32aa0e8d726a3506
Hannes Knutsson <hannes.knutsson@protonmail.com>
2021-11-18 19:11:59 +0100

    Use IPv4 only when downloading AppImage

--- scripts/install_appimages.bash
+++ scripts/install_appimages.bash
@@ -7,7 +7,7 @@ mkdir -p $APPIMG_DIR
 echo -e '\t* AppImages to install: YubiOAUTH'
 
 #Yubikey OAUTH client
-wget -q -O $APPIMG_DIR/yubioauth https://developers.yubico.com/yubioath-desktop/Releases/yubioath-desktop-latest-linux.AppImage
+wget -4 -q -O $APPIMG_DIR/yubioauth https://developers.yubico.com/yubioath-desktop/Releases/yubioath-desktop-latest-linux.AppImage
 
 chmod u+x $APPIMG_DIR/*
 
