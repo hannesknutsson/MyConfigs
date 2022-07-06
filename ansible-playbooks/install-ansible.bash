7df43ff113e5521afd0f3ebc6839eded1cb77e9d
Hannes Knutsson <hannes.knutsson@waya.se>
2022-07-06 12:21:48 +0200

    Simplify installing ansible

--- ansible-playbooks/install-ansible.bash
+++ ansible-playbooks/install-ansible.bash
@@ -1,10 +1,5 @@
 #!/bin/bash
-sudo apt install python3 curl
-sudo yum install python3 curl
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python3 get-pip.py --user
-rm get-pip.py
+sudo apt install python3-pip
+sudo yum install python3-pip
 python3 -m pip install --user ansible
 python3 -m pip install --upgrade --user ansible
-ansible --version | grep core
-python3 -m pip show ansible
